4202b9fb21fc48259333eb0e6f6d9b8ee2b175a3
Jhonatan <Jhonatan_M@web-1.laopinion>
2017-04-12 16:34:02 -0500

    actualizacion

---
+++ page--taxonomy--term--507.tpl.php
@@ -0,0 +1,25 @@
+<section id="contacto" class="container">
+  <h2>Contácto</h2>
+  <article id="info">
+    <div class="direccion">
+      <h3>Direción</h3>
+      <i class="icono"></i>
+      <img src="<?php print base_path(); ?>sites/default/themes/deleitese/images/contactoMapa.png" alt="mapa">
+      <p>Avenida 4 con calle 16 - 12 edificio La opinión, Cúcuta Colombia</p>
+      <div class="telefono">
+        <i class="iconTel"></i>
+        <p>(+57) (7) 5829999 - Fax (+57) (7) 5831502</p>
+      </div>
+    </div>
+    <div id="redesContacto">
+       <ul class="redes">
+        <div class="triLeft"></div>
+        <li class="facebook"><a href="https://www.facebook.com/deleitese.co" target="_blank" style="border:0px;"></a></li>
+        <li class="twitter"><a href="https://www.twitter.com/deleitese_co" target="_blank" style="border:0px;"></a></li>
+        <li class="instagram"><a href="https://www.instagram.com/deleitese_co" target="_blank" style="border:0px;"></a></li>
+        <li class="youtube"><a href="https://www.youtube.com/user/deleitese_co" target="_blank" style="border:0px;"></a></li>
+        <div class="triRight"></div>
+      </ul>
+    </div>
+  </article>
+</section>
